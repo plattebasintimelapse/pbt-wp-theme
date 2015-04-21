aacf92ee813258e5ac0e711d2a9d2a0720e4000e
Steven Speicher <stevenspeicher@gmail.com>
2015-04-21 09:53:07 -0500

    allow multiple author and credit byline fields

--- partials/content.php
+++ partials/content.php
@@ -4,13 +4,30 @@
  */
 ?>
 
-<?php 	$about_url = esc_url( get_permalink( get_page_by_title( 'About' ) ) );
-		$user_nicename = get_the_author_meta( 'user_nicename' ); ?>
+	<?php
+		/**
+		 * This block of code gets the About page URL: $about_url, the total number of
+		 * authors: $num_authors, and then builds arrays of the byline constructors: $authors and $author_credit.
+		 * Currently, these arrays are hard coded to only have 3 authors.
+		 */
 
-	<h5 class="story-byline">By <a href="<?php echo $about_url . '#' . $user_nicename ?>"><?php the_author(); ?></a></h5>
+		$about_url = esc_url( get_permalink( get_page_by_title( 'About' ) ) );
+		$num_authors = get_field('num_of_authors');
+
+		$authors = array( get_field('first_user'), get_field('second_user'), get_field('third_user') );
+		$author_credit = array( get_field('first_author_credit'), get_field('second_author_credit'), get_field('third_author_credit') );
+
+		echo '<div class="bylines">';
+
+		for ($x = 0; $x < $num_authors; $x++) {
+			echo '<h5 class="byline">' . $author_credit[$x] . ' <a href="' . $about_url . '#' . $authors[$x]['user_nicename'] . '">' . $authors[$x]['display_name'] . '</a></h5>';
+		}
+
+		echo '</div>';
+	?>
+
 
 
 	<?php the_content(); ?>
-
 
 	<aside class="social-media"></aside>
