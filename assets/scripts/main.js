1cefc033d48de9e4d18f4a5cf84d573aaa3b3668
Steven Speicher <stevenspeicher@gmail.com>
2015-04-23 15:46:37 -0500

    some menu stuff and other things

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -23,6 +23,10 @@ $(function() {
         }
     });
 
+    $('.toggle-info').click(function() {
+        $(this).parent().siblings('#userImgCollapse').collapse('toggle');
+    });
+
   	// $('.story-thumbnail').hover(
   	// 	function() {
 	  // 		$(this).find('.post-title').fadeOut();
@@ -54,20 +58,19 @@ $(function() {
         }, 4000);
     });
 
-    $(function() {
-        $('a[href*=#]:not([href=#])').click(function() {
-            if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-                var target = $(this.hash);
-                target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-                if (target.length) {
-                    $('html,body').animate({
-                        scrollTop: target.offset().top
-                    }, 1000);
-                    return false;
-                }
+});
+
+$(function() {
+    $('a[href*=#]:not([href=#])').click(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+            if (target.length) {
+                $('html,body').animate({
+                    scrollTop: target.offset().top
+                }, 1000);
+                return false;
             }
-        });
+        }
     });
-
-
 });
