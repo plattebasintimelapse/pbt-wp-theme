26d8e66373fa0c741b8a3db05b37fd1023e3f89c
Steven Speicher <stevenspeicher@gmail.com>
2015-04-24 14:12:57 -0500

    create helpers.php, move get bylines to helper function

---
+++ inc/helpers.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * This block of code gets the About page URL: $about_url, the total number of
+ * authors: $num_authors, and then builds arrays of the byline constructors: $authors and $author_credit.
+ * Currently, these arrays are hard coded to only have 3 authors.
+ */
+function pbt_bylines() {
+	$about_url = esc_url( get_permalink( get_page_by_title( 'About' ) ) );
+	$num_authors = get_field('num_of_authors');
+
+	$authors_nicename = array( get_the_author_meta('nicename'), get_field('second_user')['user_nicename'], get_field('third_user')['user_nicename'] );
+	$authors_displayname = array( get_the_author_meta('display_name'), get_field('second_user')['display_name'], get_field('third_user')['display_name'] );
+	$author_credit = array( get_field('first_author_credit'), get_field('second_author_credit'), get_field('third_author_credit') );
+
+	$bylines = '<div class="bylines">';
+
+	for ($x = 0; $x < $num_authors; $x++) {
+		$bylines = $bylines . '<h4 class="byline">' . $author_credit[$x] . ' <a href="' . $about_url . '#' . $authors_nicename[$x] . '">' . $authors_displayname[$x] . '</a></h4>';
+	}
+
+	$bylines = $bylines . '</div>';
+
+	echo $bylines;
+}
--- partials/content.php
+++ partials/content.php
@@ -2,32 +2,8 @@
 /**
  * The main content file
  */
-?>
 
-	<?php
-		/**
-		 * This block of code gets the About page URL: $about_url, the total number of
-		 * authors: $num_authors, and then builds arrays of the byline constructors: $authors and $author_credit.
-		 * Currently, these arrays are hard coded to only have 3 authors.
-		 */
+	pbt_bylines();
+	the_content();
 
-		$about_url = esc_url( get_permalink( get_page_by_title( 'About' ) ) );
-		$num_authors = get_field('num_of_authors');
-
-		$authors = array( get_field('first_user'), get_field('second_user'), get_field('third_user') );
-		$author_credit = array( get_field('first_author_credit'), get_field('second_author_credit'), get_field('third_author_credit') );
-
-		echo '<div class="bylines">';
-
-		for ($x = 0; $x < $num_authors; $x++) {
-			echo '<h4 class="byline">' . $author_credit[$x] . ' <a href="' . $about_url . '#' . $authors[$x]['user_nicename'] . '">' . $authors[$x]['display_name'] . '</a></h4>';
-		}
-
-		echo '</div>';
-	?>
-
-
-
-	<?php the_content(); ?>
-
-	<aside class="social-media"></aside>
+	echo '<aside class="social-media"></aside>';
