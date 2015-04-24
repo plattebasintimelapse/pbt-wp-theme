de95af2f0d2cffa1639914528f0419e7f94a21bd
Steven Speicher <stevenspeicher@gmail.com>
2015-04-24 15:45:38 -0500

    echo tax of basin on post thumbnail like cats

--- partials/post-feed-thumbnail.php
+++ partials/post-feed-thumbnail.php
@@ -5,19 +5,40 @@
  * the story posts and the blog post content type.
  * It is used on the home page, story page, blog post page, and category pages.
  */
+
 ?>
 
 	<div class="post-thumbnail">
-		<?php the_post_thumbnail( 'pbt-post-thumbnail' ); ?>
+		<?php the_post_thumbnail( 'pbt-post-thumbnail' );  ?>
 
 		<div class="post-meta-box">
+
 			<?php if ( get_post_type() === 'blog_post' ) { ?>
-				<h5 class="blog-post-author">By <?php the_author_meta('display_name') ?></h5>
+
+				<h5 class="blog-post-author">By <a href="<?php echo get_author_posts_url( get_the_author_meta( 'ID' ) ); ?>"><?php the_author(); ?></a></h5>
+
 			<?php } else if ( get_post_type() === 'post' ) { ?>
-				<h5 class="post-category"><?php the_category( ' | ' ) ?></h5>
+				<h5 class="post-category">
+
+					<?php if ( is_object_in_term( $post->ID , 'basin' ) ) { //check to see if post has basin category
+
+						$terms = get_the_terms( $post->ID , 'basin' );
+
+						foreach ( $terms as $term ) {
+							echo '<a href="' . get_term_link( $term ) . '">' . $term->name . '</a> | ';
+						}
+					}
+
+					the_category( ' | ' ); ?>
+
+				</h5>
+
 			<?php } else if ( get_post_type() === 'page' ) { ?>
+
 				<h5 class="post-category">page</h5>
+
 			<?php } ?>
+
 			<h3 class="post-title"><?php the_title(); ?></h3>
 			<a class="btn btn-default read-more-btn" role="button" href="<?php the_permalink() ?>"><h6>Read More</h6></a>
 		</div>
--- partials/searched-post-feed-thumbnail.php
+++ partials/searched-post-feed-thumbnail.php
@@ -13,15 +13,21 @@
 		<div class="post-meta-box">
 
 			<?php if ( get_post_type() === 'blog_post' ) { ?>
+
 				<h4 class="blog-post-author"><small>A Notebook entry by</small> <a href="<?php echo get_author_posts_url( get_the_author_meta( 'ID' ) ); ?>"><?php the_author(); ?></a></h4>
+
 			<?php } else if ( get_post_type() === 'post' ) { ?>
+
 				<h4 class="post-category"><small>A Story filed under</small> <?php the_category( ' | ' ) ?></h4>
+
 			<?php } else if ( get_post_type() === 'page' ) { ?>
+
 				<h4 class="post-category"><small>The page</small></h4>
+
 			<?php } ?>
 
 			<h3 class="post-title"><?php the_title(); ?></h3>
-			
+
 			<h5 class="post-meta">
 				<small class="font-size-small">Posted on <?php the_date('F j, Y');
 					if ( get_post_type() !== 'blog_post' ) { ?> by <a href="<?php echo get_author_posts_url( get_the_author_meta( 'ID' ) ); ?>"><?php the_author(); ?></a><?php } ?>
