4f431e9fdc138b97c23da309738b829406220f9c
Steven Speicher <stevenspeicher@gmail.com>
2015-04-27 10:18:12 -0500

    disqus commenting system

--- partials/content.php
+++ partials/content.php
@@ -3,7 +3,10 @@
  * The main content file
  */
 
+	echo '<h6 class="text-center font-size-small"><em>Posted on ' . get_the_date( 'F j, Y' ) . '</em></h6>';
+
 	pbt_bylines();
+
 	the_content();
 
-	echo '<aside class="social-media"></aside>';
+	echo '<aside class="post-meta"></aside>';
