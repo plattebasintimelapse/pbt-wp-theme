c140edc1c77620fb24f7a09e0db4aa3df924769b
Steven Speicher <stevenspeicher@gmail.com>
2015-05-05 16:37:43 -0500

    filter p tags from generated imgs in the content editor

--- inc/filters.php
+++ inc/filters.php
@@ -5,3 +5,9 @@ function pbt_excerpt_length( $length ) {
 	return 10;
 }
 add_filter( 'excerpt_length', 'pbt_excerpt_length', 999 );
+
+function filter_ptags_on_images($content){
+   return preg_replace('/<p>\s*(<a .*>)?\s*(<img .* \/>)\s*(<\/a>)?\s*<\/p>/iU', '\1\2\3', $content);
+}
+
+add_filter('the_content', 'filter_ptags_on_images');
