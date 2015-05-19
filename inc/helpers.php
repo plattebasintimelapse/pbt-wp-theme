c6709bb4f0b28828219edc7221c5109009317c1b
Steven Speicher <stevenspeicher@gmail.com>
2015-05-19 09:13:57 -0700

    check if public email field is filled in

--- inc/helpers.php
+++ inc/helpers.php
@@ -83,7 +83,7 @@ function pbt_author_meta($curauth) {
 		echo '<div class="author-link"><a target="_blank" href="' . $curauth->user_url . '"><i class="fa fa-laptop"></i></a></div>';
 	}
 
-	if( $curauth->user_email !== '' ) {
+	if( $curauth->public_email !== '' ) {
 		echo '<div class="author-link"><a href="mailto:' . $curauth->public_email . '"><i class="fa fa-envelope-o"></i> <small>' . $curauth->public_email . '</small></a></div>';
 	}
 }
