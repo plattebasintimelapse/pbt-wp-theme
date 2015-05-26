7c43a77b8e3e4b3fa4843460a2f9c67cddf33a9a
Steven Speicher <stevenspeicher@gmail.com>
2015-05-26 12:02:42 -0500

    new fields for learning objects

--- single-learning_object.php
+++ single-learning_object.php
@@ -9,23 +9,55 @@ get_header(); ?>
 
 		<?php while ( have_posts() ) : the_post(); ?>
 
+			<div class="container container-large" style="position: relative;">
+				<div class="col-sm-8">
+					<?php the_content(); ?>
+				</div>
+				<div class="col-sm-4">
+					<div class="row">
+						<div class="col-xs-12">
+							<h4>Time:</h4>
+							<p><?php the_field('time_to_complete'); ?></p>
+						</div>
+					</div>
 
+					<div class="row">
+						<div class="col-xs-12">
+							<h4>Downloads:</h4>
+							<p>
+								<a href="<?php the_field('download'); ?>"><i class="fa fa-file-text-o fa-3x"></i></a>
+								<i class="fa fa-file-text-o fa-3x"></i>
+							</p>
+						</div>
+					</div>
 
-		<div class="container container-large" style="position: relative;">
-			<div class="col-sm-8">
-				<?php the_content(); ?>
+					<div class="row">
+						<div class="col-xs-12">
+							<h4>Standards:</h4>
+							<p>Life Science, Earth Science</p>
+						</div>
+					</div>
+
+					<div class="row">
+						<div class="col-xs-12">
+							<h4>Additional Links:</h4>
+							<p><a class="link-color-dark" href="#">National Audubon Society</a></p>
+						</div>
+					</div>
+
+					<div class="row row-little-padding">
+						<div class="col-xs-12">
+							<a class="btn btn-primary btn-ghost btn-sm" role="button" href="#"><h6>Nebraska Standards</h6></a>
+						</div>
+					</div>
+
+					<div class="row row-little-padding">
+						<div class="col-xs-12">
+							<a class="btn btn-primary btn-ghost btn-sm" role="button" href="#"><h6>Next Gen Standards</h6></a>
+						</div>
+					</div>
+				</div>
 			</div>
-			<div class="col-sm-4">
-				<h4>Time:</h4>
-				<p>30 minutes</p>
-				<h4>Standards:</h4>
-				<p>Life Science, Earth Science</p>
-				<i class="fa fa-file-text-o fa-3x"></i>
-				<i class="fa fa-file-text-o fa-3x"></i>
-				<h4>Additional Links:</h4>
-				<p>National Audubon Society</p>
-			</div>
-		</div>
 
 		<?php endwhile; ?>
 
